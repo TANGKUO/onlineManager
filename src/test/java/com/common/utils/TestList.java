5173336d89b3566886dc5a5d742ff79ab20f52d6
TANGKUO <616507752@qq.com>
2017-05-14 12:10:10 +0800

    优化单元测试

--- src/test/java/com/common/utils/TestList.java
+++ src/test/java/com/common/utils/TestList.java
@@ -4,26 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * 
- * @author Administrator
+ * @ClassName: TestList
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author 唐阔
+ * @date 2017年5月14日 下午12:06:18
  * 
  */
 public class TestList
 {
 	private static final Logger LOG = LoggerFactory.getLogger(TestList.class);
 
-	public static void main(String[] args)
+	/**
+	 * 
+	 * @Title: testCollectionUtils
+	 * @Description: TODO(测试集合工具类使用)
+	 * @param 设定文件
+	 * @return void 返回类型
+	 * @throws
+	 */
+	@Test
+	public void testCollectionUtils()
 	{
 		List<String> list = new ArrayList<String>();
 		list.add("a");
 		list.add("b");
 		if (CollectionUtils.isNotEmpty(list))
 		{
-			//System.out.println("====" + list);
+			System.out.println("====" + list);
 			LOG.info("---------" + list);
 		}
 
