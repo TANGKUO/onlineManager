4c3ee3c5494d77dbc0caf1abcaac5376741f3241
tangkuo <616507752@qq.com>
2019-09-15 14:56:16 +0800

    java并发编程实战新增修改

---
+++ src/main/java/com/tangkuo/cn/java/concurrency/in/practice/Secrets.java
@@ -0,0 +1,22 @@
+package com.tangkuo.cn.java.concurrency.in.practice2;
+
+import java.util.*;
+
+/**
+ * Secrets
+ *
+ * Publishing an object
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+class Secrets {
+    public static Set<Secret> knownSecrets;
+
+    public void initialize() {
+        knownSecrets = new HashSet<Secret>();
+    }
+}
+
+
+class Secret {
+}
---
+++ src/main/java/com/tangkuo/cn/java/concurrency/in/practice/Sequence.java
@@ -0,0 +1,18 @@
+package com.tangkuo.cn.java.concurrency.in.practice2;
+
+import net.jcip.annotations.*;
+
+/**
+ * Sequence
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@ThreadSafe
+public class Sequence {
+    @GuardedBy("this") private int nextValue;
+
+    public synchronized int getNext() {
+        return nextValue++;
+    }
+}
